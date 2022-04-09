25e374fc9688288861b11cff2c41e95f8717f167
Matias Capuano <maticapuano97@gmail.com>
2022-04-09 13:48:36 -0300

    chore(cors): add cors

--- src/shared/infra/http/app.js
+++ src/shared/infra/http/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const v1Router = require("./routes/v1");
+const swaggerDocsRouter = require("./routes/swagger-docs.route");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
+
 app.use("/v1", v1Router);
+app.use("/docs", swaggerDocsRouter);
 
 module.exports = app;
